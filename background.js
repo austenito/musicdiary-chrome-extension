899e8f9a3690c17e652ef4c05468d001005cdfc0
Austen Ito <austen.ito@gmail.com>
2013-01-12 12:47:54 -0500

    Make local request to node server

--- background.js
+++ background.js
@@ -1,12 +1,14 @@
-//run_background();
-//init();
-//setInterval("runBackground()", 1000);
-
-//function init() {
-//};
-
 $(function() {
   $(".song_title").bind("DOMSubtreeModified", function() { 
-    console.log($('.song_title').attr("href"));
+    song_url = $('.song_title').attr("href");
+
+    body = {};
+    body["service"] = "rdio";
+    body["url"] = song_url;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:3000/log", true);
+    xhr.setRequestHeader("Content-type", "application/json")
+    xhr.send(JSON.stringify(body));
   });
 });
