0e6919d82e949272f8faed58f2dbe20092e10f31
Austen Ito <austen.ito@gmail.com>
2013-01-12 14:06:32 -0500

    Only one request sent per song change

--- background.js
+++ background.js
@@ -1,14 +1,16 @@
 $(function() {
-  $(".song_title").bind("DOMSubtreeModified", function() { 
-    song_url = $('.song_title').attr("href");
+  $(".song_title").bind("DOMSubtreeModified", function(event) {
+    if(event.srcElement.text != "") {
+      song_url = $('.song_title').attr("href");
+      body = {};
+      body["service"] = "rdio";
+      body["url"] = song_url;
+      console.log(song_url);
 
-    body = {};
-    body["service"] = "rdio";
-    body["url"] = song_url;
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:3000/log", true);
-    xhr.setRequestHeader("Content-type", "application/json")
-    xhr.send(JSON.stringify(body));
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://localhost:3000/log", true);
+      xhr.setRequestHeader("Content-type", "application/json")
+      xhr.send(JSON.stringify(body));
+    }
   });
 });
