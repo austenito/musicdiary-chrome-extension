b79ee38f717ebc9caaf5e6afdd4d2fcf04ad66d1
Austen Ito <austen.ito@gmail.com>
2013-01-17 22:29:45 -0500

    Fix bug with duplicates being sent.
    - Also refactored the code into a js module.

--- background.js
+++ background.js
@@ -1,30 +1,40 @@
-var lastRequestId = 0;
-chrome.webRequest.onCompleted.addListener(function(details) {
-  var nextRequestId = lastRequestId + 1
-  if(details.url == "http://www.rdio.com/api/1/getPlaybackInfo" && details.requestId != nextRequestId) {
-    chrome.tabs.query({url: "http://www.rdio.com/*"}, function(tabs) {
-      if(tabs.length > 0) {
-        chrome.tabs.sendMessage(tabs[0].id, {request: "getSongUrl"}, function(response) {
-          this.sendSongPlayed(response.songUrl);
-        });
-      }
-    });
+var musicDiaryListener = (function() {
+  var bg = {}
+  var lastRequestId = 0;
+  var lastSongUrlPlayed = "";
+  var rdioUrl = "http://www.rdio.com/api/1/getPlaybackInfo";
+
+  chrome.webRequest.onCompleted.addListener(function(details) {
+    var nextRequestId = lastRequestId + 1
+    if (details.url == rdioUrl && nextRequestId != lastRequestId) {
+      chrome.tabs.query({url: "http://www.rdio.com/*"}, bg.getSongUrlFromTab);
+    }
+    lastRequestId = details.requestId;
+  }, {urls: ["http://www.rdio.com/*"]}, []);
+
+  bg.getSongUrlFromTab = function(tabs) {
+    if(tabs.length > 0) {
+      chrome.tabs.sendMessage(tabs[0].id, {request: "getSongUrl"}, bg.sendSongPlayed);
+    }
+  };
+
+  bg.sendSongPlayed = function(response) {
+    var songUrl = response.songUrl;
+    if(songUrl && lastSongUrlPlayed != songUrl) {
+      console.log(songUrl);
+
+      body = {};
+      body["service"] = "rdio";
+      body["url"] = songUrl;
+
+      var xhr = new XMLHttpRequest();
+      //xhr.open("POST", "http://apihackday.herokuapp.com/log", true);
+      xhr.open("POST", "http://localhost:3000/log", true);
+      xhr.setRequestHeader("Content-type", "application/json");
+      xhr.send(JSON.stringify(body));
+    }
+    lastSongUrlPlayed = songUrl;
   }
-  lastRequestId = details.requestId;
-}, {urls: ["http://www.rdio.com/*"]}, []);
 
-function sendSongPlayed(songUrl) {
-  if(songUrl) {
-    console.log(songUrl);
-
-    body = {};
-    body["service"] = "rdio";
-    body["url"] = songUrl;
-
-    var xhr = new XMLHttpRequest();
-    //xhr.open("POST", "http://apihackday.herokuapp.com/log", true);
-    xhr.open("POST", "http://localhost:3000/log", true);
-    xhr.setRequestHeader("Content-type", "application/json");
-    xhr.send(JSON.stringify(body));
-  }
-}
+  return bg;
+}());
