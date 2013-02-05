42fa27ec8838a73eafb154ad709787c220b49cb0
Austen Ito <austen.ito@gmail.com>
2013-02-05 12:05:50 -0500

    Add Top 5 Diary and Playlist

--- popup.js
+++ popup.js
@@ -8,6 +8,8 @@ $(document).ready(function() {
   $.get(serverUrl + "/check_auth", function(data){
     if(data.authenticated) {
       $("#authenticated").show();
+      getDiary();
+      getPlaylist();
     }
     else {
       $("#login").show();
@@ -17,8 +19,34 @@ $(document).ready(function() {
     $("#auth-error").show();
   });
 
+  var getDiary = function() {
+    $.get(serverUrl + "/api/diary?song_count=5", function(data) {
+      _.each(data.songs, function(element, index, list) {
+        var row = "<tr>";
+        row += "<td><img src=" + element.icon + "></td>";
+        row += "<td><a target='_blank' href='http://www.rdio.com/" + element.url + "'>" + element.name + "</a></td>";
+        row += "<td>" + element.from_now + "</td>";
+        row += "</tr>";
+        $("#diary-table tr:last").after(row);
+      });
+    });
+  }
+
+  var getPlaylist = function() {
+    $.get(serverUrl + "/api/playlist?song_count=5", function(data) {
+      _.each(data.playlist, function(element, index, list) {
+        var row = "<tr>";
+        row += "<td><img src=" + element.icon + "></td>";
+        row += "<td><a target='_blank' href='http://www.rdio.com/" + element.url + "'>" + element.name + "</a></td>";
+        row += "<td>" + element.plays + "</td>";
+        row += "</tr>";
+        $("#playlist-table tr:last").after(row);
+      });
+    });
+  }
+
   $("#signup-link").attr("href", serverUrl);
   $("#title-link").attr("href", serverUrl);
 
- chrome.extension.sendMessage({name: "popup-click"});
+  chrome.extension.sendMessage({name: "popup-click"});
 });
